90c9f68fe2ddd4049645fc50788d695fc0f4c209
Asifali <asifali.ek@valuementor.com>
2025-06-10 12:00:39 +0530

    agent updated with pause, resume and stop action apis

--- scannerWebSocket/consumers.py
+++ scannerWebSocket/consumers.py
@@ -24,6 +24,8 @@ def decrypt(string):
         return None
 
 
+
+
 class DataConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
@@ -56,148 +58,185 @@ class DataConsumer(AsyncWebsocketConsumer):
                 for scan in pre_scheduled_scans:
                     agent_type = scan['agent_type']['agent_type_code']
                     agent_end_point = scan['agent_id']['agent_end_point']
+                    reference_scan_id = scan['reference_scan_id']
+                    scan_action = scan['action']
                     url = agent_end_point.strip("/")
-                    scanner_endpoint = url + "/scan"
-                    payload = {}
 
-                    if scan['scan_type'] == 'asset_scan':
-                        if agent_type == "nessus":
-                            if scan['nessus_policy'] != None:
-                                policy_type = scan['nessus_policy']['policy_name']
-                                policy_file = scan['nessus_policy']['policy_file']
-                            else:
-                                policy_type = None
-                                policy_file = None
-                            payload = {
-                                "target": scan['asset_id']['asset_name'],
-                                "policy_type": policy_type,
-                                "policy_file": policy_file,
-                                "org_ref_id": settings.ORG_ID
-                            }
-                        if agent_type == "burpsuit":
-                            asset_url_id = scan['url_id']
-                            asset_url = asset_url_id['url'] if asset_url_id is not None else scan['asset_id']['asset_name']
-                            payload = {
-                                "urls": asset_url,
-                                "org_ref_id": settings.ORG_ID
-                            }
-                        if agent_type == "masscan":
-                            if scan['schedule'] != None and scan['schedule']['scan_input_meta'] is not None:
-                                agent_meta = json.loads(scan['schedule']['scan_input_meta'])
-                            else:
-                                if scan['scan_input_meta'] is not None:
-                                    agent_meta = json.loads(scan['scan_input_meta'])
-                                else:
-                                    agent_meta = json.loads(scan['agent_id']['agent_meta'])
-                            payload = {
-                                "ip_string": scan['asset_id']['asset_name'],
-                                "ports_string": agent_meta['ports_string'],  # Load it from meta
-                                "scan_scope": scan['scan_scope'],
-                                "org_ref_id": settings.ORG_ID
-                            }
-                        if agent_type == "attack_surface":
-                            if scan['schedule'] != None and scan['schedule']['scan_input_meta'] is not None:
-                                agent_meta = json.loads(scan['schedule']['scan_input_meta'])
-                            else:
-                                if scan['scan_input_meta'] is not None:
-                                    agent_meta = json.loads(scan['scan_input_meta'])
-                                else:
-                                    agent_meta = json.loads(scan['agent_id']['agent_meta'])
-                            payload = {
-                                "ip_string": scan['asset_id']['asset_name'],
-                                "ports_string": agent_meta['ports_string'],  # Load it from meta
-                                "scan_scope": scan['scan_scope'],
-                                "org_ref_id": settings.ORG_ID
-                            }
-                        if agent_type == "wpscan":
-                            asset_url_id = scan['url_id']
-                            asset_url = asset_url_id['url'] if asset_url_id is not None else scan['asset_id']['asset_name']
-                            payload = {
-                                "target": asset_url,
-                                "org_ref_id": settings.ORG_ID
-                            }
-                        if agent_type == "dsscan":
-                            parsed_url = urlparse(scan['asset_id']['asset_name'])
-                            if parsed_url.netloc == '':
-                                main_domain = parsed_url.path.split(".")[-2] + "." + parsed_url.path.split(".")[-1]
-                            else:
-                                main_domain = parsed_url.netloc.split(".")[-2] + "." + parsed_url.netloc.split(".")[-1]
-                            payload = {
-                                "domain": main_domain,
-                                "org_ref_id": settings.ORG_ID
-                            }
-                        if agent_type == "ipreputation":
-                            pass
-                        if agent_type == "ssllabs":
-                            payload = {
-                                "target": scan['asset_id']['asset_name'],
-                                "org_ref_id": settings.ORG_ID
-                            }
-                        if agent_type == "zap":
-                            asset_url_id = scan['url_id']
-                            asset_url = asset_url_id['url'] if asset_url_id is not None else scan['asset_id']['asset_name']
-                            payload = {
-                                "target": asset_url,
-                                "org_ref_id": settings.ORG_ID
-                            }
+                    if scan_action == 'start':
+                        scanner_endpoint = url + "/scan"
+                    elif scan_action == 'pause':
+                        url_part = f"/scan/{reference_scan_id}/pause/"
+                        scanner_endpoint = url + url_part
+                    elif scan_action == 'resume':
+                        url_part = f"/scan/{reference_scan_id}/resume/"
+                        scanner_endpoint = url + url_part
+                    elif scan_action == 'stop':
+                        url_part = f"/scan/{reference_scan_id}/stop/"
+                        scanner_endpoint = url + url_part
                     else:
-                        if agent_type == "nessus":
-                            if scan['nessus_policy'] != None:
-                                policy_type = scan['nessus_policy']['policy_name']
-                                policy_file = scan['nessus_policy']['policy_file']
-                            else:
-                                policy_type = None
-                                policy_file = None
+                        scanner_endpoint = url + "/scan"
 
-                            payload = {
-                                "target": scan['target'],
-                                "policy_type": policy_type,
-                                "policy_file": policy_file,
-                                "org_ref_id": settings.ORG_ID,
-                                "scan_type": "network_scan"
-                            }
-                        if agent_type == "burpsuit":
-                            target = scan['target']
-                            payload = {
-                                "urls": target,
-                                "org_ref_id": settings.ORG_ID,
-                                "scan_type": "network_scan"
-                            }
-                        if agent_type == "zap":
-                            scan_input_meta = json.loads(scan['scan_input_meta'])
-                            username = scan_input_meta.get("username")
-                            password = decrypt(scan_input_meta.get("password"))
-
-                            payload = {
-                                "target": scan['target'],
-                                "org_ref_id": settings.ORG_ID,
-                                "scan_type": "network_scan",
-                                "postman_file": scan['file_path'],
-                                "username": username,
-                                "password": password
-                            }
-                    
                     scanner_response_data = {}
                     scanner_response_data['scan_id'] = scan['scan_id']
                     scanner_response_data['scan_type'] = scan['scan_type']
-                    try:
-                        result = requests.post(scanner_endpoint, json=payload, timeout=15)
-                        if result is not None:
-                            scan_result = result.json()
+                    scanner_response_data['action'] = scan_action
 
-                            # Create scan data to update secusy
-                            scanner_response_data['result_file'] = scan_result['result_url']
-                            scanner_response_data['errors'] = scan_result['errors']
-                            scanner_response_data['reference_scan_id'] = scan_result['scan_id']
-                            scanner_response_data['scan_result_meta'] = scan_result
+                    if scan_action == 'start':
+                        payload = {}
+                        if scan['scan_type'] == 'asset_scan':
+                            if agent_type == "nessus":
+                                if scan['nessus_policy'] != None:
+                                    policy_type = scan['nessus_policy']['policy_name']
+                                    policy_file = scan['nessus_policy']['policy_file']
+                                else:
+                                    policy_type = None
+                                    policy_file = None
+                                payload = {
+                                    "target": scan['asset_id']['asset_name'],
+                                    "policy_type": policy_type,
+                                    "policy_file": policy_file,
+                                    "org_ref_id": settings.ORG_ID
+                                }
+                            if agent_type == "burpsuit":
+                                asset_url_id = scan['url_id']
+                                asset_url = asset_url_id['url'] if asset_url_id is not None else scan['asset_id']['asset_name']
+                                payload = {
+                                    "urls": asset_url,
+                                    "org_ref_id": settings.ORG_ID
+                                }
+                            if agent_type == "masscan":
+                                if scan['schedule'] != None and scan['schedule']['scan_input_meta'] is not None:
+                                    agent_meta = json.loads(scan['schedule']['scan_input_meta'])
+                                else:
+                                    if scan['scan_input_meta'] is not None:
+                                        agent_meta = json.loads(scan['scan_input_meta'])
+                                    else:
+                                        agent_meta = json.loads(scan['agent_id']['agent_meta'])
+                                payload = {
+                                    "ip_string": scan['asset_id']['asset_name'],
+                                    "ports_string": agent_meta['ports_string'],  # Load it from meta
+                                    "scan_scope": scan['scan_scope'],
+                                    "org_ref_id": settings.ORG_ID
+                                }
+                            if agent_type == "attack_surface":
+                                if scan['schedule'] != None and scan['schedule']['scan_input_meta'] is not None:
+                                    agent_meta = json.loads(scan['schedule']['scan_input_meta'])
+                                else:
+                                    if scan['scan_input_meta'] is not None:
+                                        agent_meta = json.loads(scan['scan_input_meta'])
+                                    else:
+                                        agent_meta = json.loads(scan['agent_id']['agent_meta'])
+                                payload = {
+                                    "ip_string": scan['asset_id']['asset_name'],
+                                    "ports_string": agent_meta['ports_string'],  # Load it from meta
+                                    "scan_scope": scan['scan_scope'],
+                                    "org_ref_id": settings.ORG_ID
+                                }
+                            if agent_type == "wpscan":
+                                asset_url_id = scan['url_id']
+                                asset_url = asset_url_id['url'] if asset_url_id is not None else scan['asset_id']['asset_name']
+                                payload = {
+                                    "target": asset_url,
+                                    "org_ref_id": settings.ORG_ID
+                                }
+                            if agent_type == "dsscan":
+                                parsed_url = urlparse(scan['asset_id']['asset_name'])
+                                if parsed_url.netloc == '':
+                                    main_domain = parsed_url.path.split(".")[-2] + "." + parsed_url.path.split(".")[-1]
+                                else:
+                                    main_domain = parsed_url.netloc.split(".")[-2] + "." + parsed_url.netloc.split(".")[-1]
+                                payload = {
+                                    "domain": main_domain,
+                                    "org_ref_id": settings.ORG_ID
+                                }
+                            if agent_type == "ipreputation":
+                                pass
+                            if agent_type == "ssllabs":
+                                payload = {
+                                    "target": scan['asset_id']['asset_name'],
+                                    "org_ref_id": settings.ORG_ID
+                                }
+                            if agent_type == "zap":
+                                asset_url_id = scan['url_id']
+                                asset_url = asset_url_id['url'] if asset_url_id is not None else scan['asset_id']['asset_name']
+                                payload = {
+                                    "target": asset_url,
+                                    "org_ref_id": settings.ORG_ID
+                                }
+                        else:
+                            if agent_type == "nessus":
+                                if scan['nessus_policy'] != None:
+                                    policy_type = scan['nessus_policy']['policy_name']
+                                    policy_file = scan['nessus_policy']['policy_file']
+                                else:
+                                    policy_type = None
+                                    policy_file = None
+
+                                payload = {
+                                    "target": scan['target'],
+                                    "policy_type": policy_type,
+                                    "policy_file": policy_file,
+                                    "org_ref_id": settings.ORG_ID,
+                                    "scan_type": "network_scan"
+                                }
+                            if agent_type == "burpsuit":
+                                target = scan['target']
+                                payload = {
+                                    "urls": target,
+                                    "org_ref_id": settings.ORG_ID,
+                                    "scan_type": "network_scan"
+                                }
+                            if agent_type == "zap":
+                                scan_input_meta = json.loads(scan['scan_input_meta'])
+                                username = scan_input_meta.get("username")
+                                password = decrypt(scan_input_meta.get("password"))
+
+                                scanner_endpoint = url + '/api-scan'
+
+                                payload = {
+                                    "target": scan['target'],
+                                    "org_ref_id": settings.ORG_ID,
+                                    "scan_type": "network_scan",
+                                    "postman_file": scan['file_path'],
+                                    "username": username,
+                                    "password": password
+                                }
+                        
+                        try:
+                            result = requests.post(scanner_endpoint, json=payload, timeout=15)
+                            if result is not None:
+                                scan_result = result.json()
+
+                                # Create scan data to update secusy
+                                scanner_response_data['result_file'] = scan_result['result_url']
+                                scanner_response_data['errors'] = scan_result['errors']
+                                scanner_response_data['reference_scan_id'] = scan_result['scan_id']
+                                scanner_response_data['scan_result_meta'] = scan_result
+                                scanner_response_data['last_run_at'] = timezone.now().isoformat()
+                                scanner_response_data['scan_status'] = 1
+                        except Exception as e:
                             scanner_response_data['last_run_at'] = timezone.now().isoformat()
-                            scanner_response_data['scan_status'] = 1
-                    except Exception as e:
-                        scanner_response_data['last_run_at'] = timezone.now().isoformat()
-                        scanner_response_data['errors'] = "Scanner down. "+str(e)
-                        scanner_response_data['is_scan_paused'] = True
-                        scanner_response_data['scan_status'] = 3
+                            scanner_response_data['errors'] = "Scanner down. "+str(e)
+                            scanner_response_data['is_scan_paused'] = True
+                            scanner_response_data['scan_status'] = 3
+                    else:
+                        try:
+                            response = requests.post(scanner_endpoint, timeout=10)
+                            if result is not None:
+                                json_response = response.json()
 
+                                if json_response.get('status') == 'success':
+                                    if scan_action == 'pause':
+                                        scanner_response_data['scan_status'] = 7
+                                    elif scan_action == 'resume':
+                                        scanner_response_data['scan_status'] = 9 # passing 9 for handling inprogress(1) status, 
+                                    elif scan_action == 'stop':
+                                        scanner_response_data['scan_status'] = 8
+                                    else:
+                                        pass
+                        except Exception as e:
+                            print(f"Scan action error: {str(e)}.")
+                    
                     # return internal scanner response to secusy
                     try:
                         scan_endpoint = settings.SECUSY_API + '/scan/prescheduled-scan-update/'
